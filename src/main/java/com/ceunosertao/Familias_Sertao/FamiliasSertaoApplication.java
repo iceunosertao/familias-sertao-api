49790629317f1af8b5757d2a02ed64159013cb70
Matheus Farias <matheusfarias3456@hotmail.com>
2025-08-12 20:52:36 -0300

    Estruturando o projeto

---
+++ src/main/java/com/ceunosertao/Familias_Sertao/FamiliasSertaoApplication.java
@@ -0,0 +1,13 @@
+package com.ceunosertao.Familias_Sertao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamiliasSertaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamiliasSertaoApplication.class, args);
+	}
+
+}
